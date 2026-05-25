104441298fc17d071bfe90c7ac84399a2ca93ca4
Ishtiaq Robin <robin.pass36@gmail.com>
2026-05-25 10:05:42 +0600

    update next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    allowedDevOrigins: [
-        "http://localhost:5000",
-        "http://localhost:3000",
-        "http://192.168.0.177:5000",
-        "http://192.168.0.177:3000"
-    ],
-
     experimental: {
         serverActions: {
-            bodySizeLimit: "5mb", // তোমার max file size অনুযায়ী
+            bodySizeLimit: "5mb",
         },
     },
 
     images: {
-        unoptimized: true, // Allow query strings for cache-busting
-        // formats: ["image/avif", "image/webp"],
+        unoptimized: true,
         remotePatterns: [
-            // accept any domains
-            { protocol: "https", hostname: "*" },
-            { protocol: "http", hostname: "*" },
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+            {
+                protocol: "http",
+                hostname: "**",
+            },
         ]
     }
 };
