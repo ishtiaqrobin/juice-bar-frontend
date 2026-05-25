94d56e28ad2503164f7252fabb21db5cd376eddd
Ishtiaq Robin <robin.pass36@gmail.com>
2026-05-25 22:28:07 +0600

    comment proxy macher code

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,46 +1,46 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import { sessionService } from "./services/session.service";
-// import { Roles } from "./constants/roles";
+import { NextRequest, NextResponse } from "next/server";
+import { sessionService } from "./services/session.service";
+import { Roles } from "./constants/roles";
 
-// export async function proxy(request: NextRequest) {
-//   const pathname = request.nextUrl.pathname;
+export async function proxy(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
 
-//   let isAuthenticated = false;
-//   let isAdmin = false;
+  let isAuthenticated = false;
+  let isAdmin = false;
 
-//   // const { data } = await sessionService.getSession();
-//   // console.log("session data", data);
+  // const { data } = await sessionService.getSession();
+  // console.log("session data", data);
 
-//   // Server-side session check — pass request so cookies are forwarded
+  // Server-side session check — pass request so cookies are forwarded
 
-//   try {
-//     const { data } = await sessionService.getSessionFromRequest(request);
+  try {
+    const { data } = await sessionService.getSessionFromRequest(request);
 
-//     console.log("session data", data);
+    console.log("session data", data);
 
-//     if (data?.user) {
-//       isAuthenticated = true;
-//       isAdmin = data.user.role === Roles.admin;
-//     }
-//   } catch (error) {
-//     console.error("Session fetch error in middleware:", error);
-//   }
+    if (data?.user) {
+      isAuthenticated = true;
+      isAdmin = data.user.role === Roles.admin;
+    }
+  } catch (error) {
+    console.error("Session fetch error in middleware:", error);
+  }
 
-//   //* Redirect if authenticated user tries to access /login
-//   if (pathname === "/login") {
-//     if (isAuthenticated) {
-//       return NextResponse.redirect(new URL("/", request.url));
-//     }
-//     return NextResponse.next();
-//   }
+  //* Redirect if authenticated user tries to access /login
+  if (pathname === "/login") {
+    if (isAuthenticated) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+    return NextResponse.next();
+  }
 
-//   //* User is not authenticated at all
-//   if (!isAuthenticated) {
-//     return NextResponse.redirect(new URL("/login", request.url));
-//   }
+  //* User is not authenticated at all
+  if (!isAuthenticated) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 
-//   //* User is authenticated and role = ADMIN
-//   //* User can not visit user dashboard
+  //* User is authenticated and role = ADMIN
+  //* User can not visit user dashboard
 //   if (isAdmin && pathname.startsWith("/dashboard")) {
 //     return NextResponse.redirect(new URL("/admin-dashboard", request.url));
 //   }
@@ -51,15 +51,15 @@
 //     return NextResponse.redirect(new URL("/dashboard", request.url));
 //   }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
 
-// export const config = {
-//   matcher: [
-//     "/dashboard",
-//     "/dashboard/:path*",
-//     "/admin-dashboard",
-//     "/admin-dashboard/:path*",
-//     "/login",
-//   ],
-// };
+export const config = {
+  matcher: [
+    // "/dashboard",
+    // "/dashboard/:path*",
+    // "/admin-dashboard",
+    // "/admin-dashboard/:path*",
+    // "/login",
+  ],
+};
